a6df917d16cc5d37b649c3a2f1df36a35e37ef72
julianomedeirosmachado <julianomachado@protonmail.com>
2021-09-01 09:56:15 -0300

    Adição de vetores

--- trabalho.c
+++ trabalho.c
@@ -3,23 +3,24 @@
 
 int main()
 {
-    int volume,comprimentosala,largurasala,pedireitosala,janelas,areajanelas,portas,areaportas,pessoas,equipamentos,posicao, entreandares, sobtelhado, metragemjanela,posicaojanela;
-    int solmanhacc, soltardecc, solmanhasc, soltardesc,vidrossombra, areafinalportas, kcalportas, kcalpessoas, kcalequipamentos;
+    int pessoas,equipamentos,posicao, entreandares, sobtelhado,posicaojanela;
+    int solmanhacc, soltardecc, solmanhasc, soltardesc,vidrossombra, kcalportas, kcalpessoas, kcalequipamentos;
     float formulalocal, kcal, formulajanela, formulaequipamentos, formulabtu;
     kcal = 0; // inicializando a variável
+    int dimensaosala[4],janelas[3],portas[3];
     
     // Dados de entrada
 
     printf("Dimensionamento de Ar Condicionado\n");
     printf("\nInsira os dados que serao pedidos a seguir\n");
     printf("\nQual o comprimento da sala ou escritorio? ");
-    scanf("%d", &comprimentosala);
+    scanf("%d", &dimensaosala[1]);
     printf("\nQual a largura? ");
-    scanf("%d", &largurasala);
+    scanf("%d", &dimensaosala[2]);
     printf("\nQual a distancia entre o chao e o teto? ");
-    scanf("%d", &pedireitosala);
+    scanf("%d", &dimensaosala[3]);
 
-    volume= comprimentosala * largurasala * pedireitosala;  // Calculo do volume
+    dimensaosala[4]= dimensaosala[1] * dimensaosala[2] * dimensaosala[3];  // Calculo do volume
 
     posicao = 0;
 
@@ -33,23 +34,23 @@ int main()
 
     switch(posicao){ // implementando função da kcal no código
         case 1:
-            formulalocal = 16 * volume;
+            formulalocal = 16 * dimensaosala[4];
             break;
         case 2:
-            formulalocal = 22.3 * volume;
+            formulalocal = 22.3 * dimensaosala[4];
             break;
     }
 
     printf("\nO gasto em kcal dessa posicao e: %.2f\n", formulalocal);
 
     printf("\nQuantas janelas se encontram no recinto? ");
-    scanf("%d", &janelas);
+    scanf("%d", &janelas[1]);
     printf("\nQual a area ocupada por cada janela? ");
-    scanf("%d", &areajanelas);
+    scanf("%d", &janelas[2]);
 
     posicaojanela = 0; // inicializando a variável
 
-    metragemjanela = janelas * areajanelas; // metros quadrados totais para saber o valor da tabela
+    janelas[3] = janelas[1] * janelas[2]; // metros quadrados totais para saber o valor da tabela
 
     while (posicaojanela>5 || posicaojanela <= 0){ // escolha da posição para o usuário
     printf("\nQual e a exposicao ao sol das janelas?\n");
@@ -64,19 +65,19 @@ int main()
 
     switch(posicaojanela){ // implementando a formula no código
         case 1:
-            formulajanela = 160 * metragemjanela;
+            formulajanela = 160 * janelas[3];
             break;
         case 2:
-            formulajanela = 212 * metragemjanela;
+            formulajanela = 212 * janelas[3];
             break;
         case 3:
-            formulajanela = 222 * metragemjanela;
+            formulajanela = 222 * janelas[3];
             break;
         case 4:
-            formulajanela = 410 * metragemjanela;
+            formulajanela = 410 * janelas[3];
             break;
         case 5:
-            formulajanela = 37 * metragemjanela;
+            formulajanela = 37 * janelas[3];
             break;
     }
 
@@ -85,13 +86,13 @@ int main()
     printf("\nO gasto calorico ocasionado pelas janela e: %.2f\n", formulajanela);
 
     printf("\nQuantas portas existem no recinto? ");
-    scanf("%d", &portas);
+    scanf("%d", &portas[1]);
     printf("\nQual a area das portas? ");
-    scanf("%d", &areaportas);
+    scanf("%d", &portas[2]);
 
-    areafinalportas = portas * areaportas;
+    portas[3] = portas[1] * portas[2];
 
-    kcalportas = 125 * areafinalportas;
+    kcalportas = 125 * portas[3];
 
     printf("\no gasto calorico ocasionado pela(s) porta(s) e %d\n", kcalportas);
     kcal += kcalportas;
