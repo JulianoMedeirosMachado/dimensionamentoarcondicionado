37f826a6b8d82449f9aaa4803c1c5c771eb74d2f
julianomedeirosmachado <julianomachado@protonmail.com>
2021-09-01 09:34:14 -0300

    Implementação de fórmulas

--- trabalho.c
+++ trabalho.c
@@ -3,19 +3,20 @@
 
 int main()
 {
-    int volume,comprimentosala,largurasala,pedireitosala,janelas,areajanelas,portas,areaportas,pessoas,equipamentos,posicao, kcal, entreandares, sobtelhado, metragemjanela,posicaojanela;
+    int volume,comprimentosala,largurasala,pedireitosala,janelas,areajanelas,portas,areaportas,pessoas,equipamentos,posicao, entreandares, sobtelhado, metragemjanela,posicaojanela;
     int solmanhacc, soltardecc, solmanhasc, soltardesc,vidrossombra, areafinalportas, kcalportas, kcalpessoas, kcalequipamentos;
+    float formulalocal, kcal, formulajanela, formulaequipamentos, formulabtu;
     kcal = 0; // inicializando a variável
     
     // Dados de entrada
 
     printf("Dimensionamento de Ar Condicionado\n");
     printf("\nInsira os dados que serao pedidos a seguir\n");
-    printf("\nQual o comprimento da sala ou escritorio?\n");
+    printf("\nQual o comprimento da sala ou escritorio? ");
     scanf("%d", &comprimentosala);
-    printf("\nQual a largura?\n");
+    printf("\nQual a largura? ");
     scanf("%d", &largurasala);
-    printf("\nQual a distancia entre o chao e o teto?\n");
+    printf("\nQual a distancia entre o chao e o teto? ");
     scanf("%d", &pedireitosala);
 
     volume= comprimentosala * largurasala * pedireitosala;  // Calculo do volume
@@ -26,334 +27,98 @@ int main()
     printf("\nQual a posicao do recinto?");
     printf("\n1 - Entre andares\n");
     printf("2 - Sob telhado\n");
+    printf("\nDigite uma das opcoes acima: ");
     scanf("%d", &posicao);
     }
 
-
-    switch(volume){ // implementando a tabela no código
-        case 30 :
-            entreandares = 480;
-            sobtelhado = 670;
+    switch(posicao){ // implementando função da kcal no código
+        case 1:
+            formulalocal = 16 * volume;
             break;
-        case 33 :
-            entreandares = 530;
-            sobtelhado = 740;
+        case 2:
+            formulalocal = 22.3 * volume;
             break;
-        case 36 :
-            entreandares = 580;
-            sobtelhado = 800;
-            break;
-        case 39 :
-            entreandares = 620;
-            sobtelhado = 870;
-            break;
-        case 42 :
-            entreandares = 670;
-            sobtelhado = 940;
-            break;
-        case 45 : 
-            entreandares = 720;
-            sobtelhado = 1000;
-            break;
-        case 48 :
-            entreandares = 770;
-            sobtelhado = 1070;
-            break;
-        case 51 :
-            entreandares = 816;
-            sobtelhado = 1140;
-            break;
-        case 54 :
-            entreandares = 864;
-            sobtelhado = 1200;
-            break;
-        case 57 :
-            entreandares = 910;
-            sobtelhado = 1270;
-            break;
-        case 60 :
-            entreandares = 960;
-            sobtelhado = 1340;
-            break;
-        case 63 :
-            entreandares = 1010;
-            sobtelhado = 1410;
-            break;
-        case 66 :
-            entreandares = 1060;
-            sobtelhado = 1470;
-            break;
-        case 69 :
-            entreandares = 1100;
-            sobtelhado = 1540;
-            break;
-        case 72 :
-            entreandares = 1150;
-            sobtelhado = 1610;
-            break;
-        case 75 :
-            entreandares = 1200;
-            sobtelhado = 1680;
-            break;
-        case 78 :
-            entreandares = 1250;
-            sobtelhado = 1740;
-            break;
-        case 81 :
-            entreandares = 1300;
-            sobtelhado = 1810;
-            break;
-        case 84 :
-            entreandares = 1340;
-            sobtelhado = 1880;
-            break;
-        case 87 :
-            entreandares = 1390;
-            sobtelhado = 1940;
-            break;
-        case 90 :
-            entreandares = 1440;
-            sobtelhado = 2010;
-            break;
-        }
-
-
-    if (posicao == 1){
-        kcal += entreandares;
-        printf("O gasto em kcal dessa posicao e: %d", entreandares);
-    } else {
-        kcal += sobtelhado;
-        printf("O gasto em kcal dessa posicao e: %d", sobtelhado);
     }
 
+    printf("\nO gasto em kcal dessa posicao e: %.2f\n", formulalocal);
 
-    printf("\nQuantas janelas se encontram no recinto?\n");
+    printf("\nQuantas janelas se encontram no recinto? ");
     scanf("%d", &janelas);
-    printf("\nQual a area ocupada por cada janela?\n");
+    printf("\nQual a area ocupada por cada janela? ");
     scanf("%d", &areajanelas);
 
-    posicaojanela = 0;
+    posicaojanela = 0; // inicializando a variável
 
     metragemjanela = janelas * areajanelas; // metros quadrados totais para saber o valor da tabela
 
-    while (posicaojanela>5 || posicao <= 0){ // escolha da posição para o usuário
+    while (posicaojanela>5 || posicaojanela <= 0){ // escolha da posição para o usuário
     printf("\nQual e a exposicao ao sol das janelas?\n");
     printf("\n 1- sol pela manha com cortina");
     printf("\n 2- sol a tarde com cortina");
     printf("\n 3- sol pela manha sem cortina");
     printf("\n 4- sol a tarde sem cortina");
     printf("\n 5- vidros na sombra\n");
+    printf("\nDigite uma opcao acima: ");
     scanf("%d", &posicaojanela);
     }
 
-    switch(metragemjanela){ // implementando a tabela no código
+    switch(posicaojanela){ // implementando a formula no código
         case 1:
-            solmanhacc = 160;
-            soltardecc = 212;
-            solmanhasc = 222;
-            soltardesc = 410;
-            vidrossombra = 37;
+            formulajanela = 160 * metragemjanela;
             break;
         case 2:
-            solmanhacc = 320;
-            soltardecc = 424;
-            solmanhasc = 444;
-            soltardesc = 820;
-            vidrossombra = 74;
+            formulajanela = 212 * metragemjanela;
             break;
         case 3:
-            solmanhacc = 480;
-            soltardecc = 636;
-            solmanhasc = 666;
-            soltardesc = 1230;
-            vidrossombra = 110;
+            formulajanela = 222 * metragemjanela;
             break;
         case 4:
-            solmanhacc = 640;
-            soltardecc = 848;
-            solmanhasc = 888;
-            soltardesc = 1640;
-            vidrossombra = 148;
+            formulajanela = 410 * metragemjanela;
             break;
         case 5:
-            solmanhacc = 800;
-            soltardecc = 1060;
-            solmanhasc = 1110;
-            soltardesc = 2050;
-            vidrossombra = 185;
-            break;
-        case 6:
-            solmanhacc = 960;
-            soltardecc = 1272;
-            solmanhasc = 1332;
-            soltardesc = 2460;
-            vidrossombra = 222;
-            break;
-        case 7:
-            solmanhacc = 1120;
-            soltardecc = 1484;
-            solmanhasc = 1554;
-            soltardesc = 2870;
-            vidrossombra = 260;
-            break;
-        case 8:
-            solmanhacc = 1280;
-            soltardecc = 1696;
-            solmanhasc = 1777;
-            soltardesc = 3280;
-            vidrossombra = 295;
-            break;
-        case 9:
-            solmanhacc = 1440;
-            soltardecc = 1908;
-            solmanhasc = 1998;
-            soltardesc = 3960;
-            vidrossombra = 330;
-            break;
-        case 10:
-            solmanhacc = 1600;
-            soltardecc = 2120;
-            solmanhasc = 2220;
-            soltardesc = 4100;
-            vidrossombra = 370;
+            formulajanela = 37 * metragemjanela;
             break;
     }
 
-    if (posicaojanela == 1){
-        kcal += solmanhacc;
-        printf("o gasto calorico ocasionado pelas janelas e %d", solmanhacc);
-    } else if (posicaojanela == 2){
-        kcal += soltardecc;
-        printf("o gasto calorico ocasionado pelas janelas e %d", soltardecc);
-    } else if (posicaojanela == 3){
-        kcal += solmanhasc;
-        printf("o gasto calorico ocasionado pelas janelas e %d", solmanhasc);
-    } else if (posicaojanela == 4){
-        kcal += soltardesc;
-        printf("o gasto calorico ocasionado pelas janelas e %d", soltardesc);
-    } else if (posicaojanela == 5){
-        kcal += vidrossombra;
-        printf("o gasto calorico ocasionado pelas janelas e %d", vidrossombra);
-    }
+    kcal += formulajanela;
 
-    printf("\nQuantas portas existem no recinto?\n");
+    printf("\nO gasto calorico ocasionado pelas janela e: %.2f\n", formulajanela);
+
+    printf("\nQuantas portas existem no recinto? ");
     scanf("%d", &portas);
-    printf("\nQual a area das portas?\n");
+    printf("\nQual a area das portas? ");
     scanf("%d", &areaportas);
 
     areafinalportas = portas * areaportas;
 
-    switch(areafinalportas){ //implementando a tabela no código
-        case 1:
-            kcalportas = 125;
-            break;
-        case 2:
-            kcalportas = 250;
-            break;
-        case 3:
-            kcalportas = 375;
-            break;
-        case 4:
-            kcalportas = 500;
-            break;
-        case 5:
-            kcalportas = 625;
-            break;
-        case 6:
-            kcalportas = 750;
-            break;
-        case 7:
-            kcalportas = 875;
-            break;
-        case 8:
-            kcalportas = 1000;
-            break;
-        case 9:
-            kcalportas = 1125;
-            break;
-        case 10:
-            kcalportas = 1250;
-            break;
-    }
+    kcalportas = 125 * areafinalportas;
 
-    printf("o gasto calorico ocasionado pela(s) porta(s) e %d", kcalportas);
+    printf("\no gasto calorico ocasionado pela(s) porta(s) e %d\n", kcalportas);
     kcal += kcalportas;
 
-    printf("\nQual o numero de pessoas que fica no recinto?\n");
+    printf("\nQual o numero de pessoas que fica no recinto? ");
     scanf("%d", &pessoas);
 
-    switch(pessoas){ //implementando a tabela no código
-        case 1:
-            kcalpessoas = 125;
-            break;
-        case 2:
-            kcalpessoas = 250;
-            break;
-        case 3:
-            kcalpessoas = 375;
-            break;
-        case 4:
-            kcalpessoas = 500;
-            break;
-        case 5:
-            kcalpessoas = 625;
-            break;
-        case 6:
-            kcalpessoas = 750;
-            break;
-        case 7:
-            kcalpessoas = 875;
-            break;
-        case 8:
-            kcalpessoas = 1000;
-            break;
-        case 9:
-            kcalpessoas = 1125;
-            break;
-        case 10:
-            kcalpessoas = 1250;
-            break;
-    }
+    kcalpessoas = 125 * pessoas;
+
+    printf("\nO gasto calorico ocasionado por esse numero de pessoas e: %d\n", kcalpessoas);
 
     kcal += kcalpessoas;
 
-        printf("\nQuantos Watts todos os eletronicos do recinto consomem?\n");
-        scanf(" %d", &equipamentos);
+    printf("\nQuantos Watts todos os eletronicos do recinto consomem? ");
+    scanf(" %d", &equipamentos);
 
-        switch(equipamentos){
-            case 50:
-                kcalequipamentos = 45;
-                break;
-            case 100:
-                kcalequipamentos = 90;
-                break;
-            case 150:
-                kcalequipamentos = 135;
-                break;
-            case 200:
-                kcalequipamentos = 180;
-                break;
-            case 250:
-                kcalequipamentos = 225;
-                break;
-            case 300:
-                kcalequipamentos = 270;
-                break;
-            case 350:
-                kcalequipamentos = 315;
-                break;
-            case 400:
-                kcalequipamentos = 360;
-                break;
-            case 450:
-                kcalequipamentos = 405;
-                break;
-            case 500:
-                kcalequipamentos = 450;
-                break;
-    }
+    formulaequipamentos = 0.9 * equipamentos;
 
-    printf("o gasto calorico ocasionado pelos eletronicos e %d", kcalequipamentos);
-    kcal += kcalequipamentos;
+    kcal += formulaequipamentos;
+
+    printf("\no gasto calorico ocasionado pelos eletronicos e %.2f\n", formulaequipamentos);
+    printf("\no numero de kcal totais ao final foi: %.2f\n", kcal);
+
+    //convertendo para BTUs
+
+    formulabtu = 3.92 * kcal;
+
+    printf("\nO resultado em BTUs foi: %.0f\n", formulabtu);
 
 }
